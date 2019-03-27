f15bef6d97ee9a668e4542eef4439d1f5abdca4e
JamesGagnon1 <43007253+JamesGagnon1@users.noreply.github.com>
2019-03-27 10:23:23 -0400

    the game

---
+++ Car Tron/Assets/Scripts/Builder.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour {
+
+	int TypePick;
+	int[] Parts = {0, 0, 0, 0, 0};
+	int[] PartShow = {0, 0, 0, 0, 0};
+	public int[] PartLength;
+	public GameObject BuildCanvas;
+	public GameObject GameCanvas;
+
+	void Start () {
+		TypePick = 0;
+	}
+	
+
+	void Update () {
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Body")) {
+			if (G.name == "Body (" + Parts[0] + ")") {
+				G.transform.position = transform.position;
+				G.transform.rotation = transform.rotation;
+				G.transform.SetParent(transform);
+			} else {
+				G.transform.SetParent(null);
+				G.transform.position = new Vector3 (0, -100, 0);
+			}
+		}
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Hat")) {
+			if (G.name == "Hat (" + Parts[1] + ")") {
+				G.transform.position = GameObject.Find("Body (" + Parts[0] + ")/HatPos").transform.position;
+				G.transform.rotation = GameObject.Find("Body (" + Parts[0] + ")/HatPos").transform.rotation;
+				G.transform.SetParent(transform);
+			} else {
+				G.transform.SetParent(null);
+				G.transform.position = new Vector3 (0, -100, 0);
+			}
+		}
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Nose")) {
+			if (G.name == "Nose (" + Parts[2] + ")") {
+				G.transform.position = GameObject.Find("Body (" + Parts[0] + ")/NosePos").transform.position;
+				G.transform.rotation = GameObject.Find("Body (" + Parts[0] + ")/NosePos").transform.rotation;
+				G.transform.SetParent(transform);
+			} else {
+				G.transform.SetParent(null);
+				G.transform.position = new Vector3 (0, -100, 0);
+			}
+		}
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Wing")) {
+			if (G.name == "Wing (" + Parts[3] + ")") {
+				G.transform.position = GameObject.Find("Body (" + Parts[0] + ")").transform.position;
+				G.transform.rotation = GameObject.Find("Body (" + Parts[0] + ")").transform.rotation;
+				G.transform.SetParent(transform);
+			} else {
+				G.transform.SetParent(null);
+				G.transform.position = new Vector3 (0, -100, 0);
+			}
+		}
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Engine")) {
+			if (G.name == "Engine (" + Parts[4] + ")") {
+				G.transform.position = GameObject.Find("Body (" + Parts[0] + ")").transform.position;
+				G.transform.rotation = GameObject.Find("Body (" + Parts[0] + ")").transform.rotation;
+				G.transform.SetParent(transform);
+			} else {
+				G.transform.SetParent(null);
+				G.transform.position = new Vector3 (0, -100, 0);
+			}
+		}
+	
+		transform.RotateAround(transform.position, transform.up, Time.deltaTime * 50);
+		transform.position += transform.up * Mathf.Sin (Time.time * 3) * Time.deltaTime ;
+	}
+	public void BodyBtn() {
+		TypePick = 0;
+	}
+	public void HatBtn() {
+		TypePick = 1;
+	}
+	public void NoseBtn() {
+		TypePick = 2;
+	}
+	public void WingBtn() {
+		TypePick = 3;
+	}
+	public void EngineBtn() {
+		TypePick = 4;
+	}
+	public void Right() {
+		PartShow[TypePick]++;
+		if (PartShow[TypePick] + 1 > PartLength[TypePick]) {
+			PartShow[TypePick] -= PartLength[TypePick];
+		}
+		Parts[TypePick] = PartShow[TypePick];
+	}
+	public void Left() {
+		PartShow[TypePick]--;
+		if (PartShow[TypePick] < 0) {
+			PartShow[TypePick] = PartLength[TypePick] - 1;
+		}
+		Parts[TypePick] = PartShow[TypePick];
+	}
+
+	public void StartBtn() {
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Body")) {
+			if (G.transform.parent == null) {
+				Destroy(G);
+			}
+		}
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Hat")) {
+			if (G.transform.parent == null) {
+				Destroy(G);
+			}
+		}
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Nose")) {
+			if (G.transform.parent == null) {
+				Destroy(G);
+			}
+		}
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Wing")) {
+			if (G.transform.parent == null) {
+				Destroy(G);
+			}
+		}
+		foreach (GameObject G in GameObject.FindGameObjectsWithTag("Engine")) {
+			if (G.transform.parent == null) {
+				Destroy(G);
+			}
+		}
+		BuildCanvas.SetActive(false);
+		GameCanvas.SetActive(true);
+		GetComponent<CarMove>().enabled = true;
+		GameObject.Find("Main Camera").GetComponent<Cam>().enabled = true;
+		GetComponent<Builder>().enabled = false;
+	}
+}
---
+++ Car Tron/Assets/Scripts/CarMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour {
+
+	public float Speed;
+	public float BrakePower;
+	public float first;
+	public float second;
+	public GameObject Menu;
+	Rigidbody RB;
+
+	void Start () {
+		RB = GetComponent <Rigidbody>();
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		RB.centerOfMass = new Vector3(0, -0.5f, 0.5f);
+		transform.position = new Vector3 (0, 1f, 0);
+		transform.eulerAngles = new Vector3 (0, 0, 0);
+		foreach (WheelCollider G in GetComponentsInChildren<WheelCollider>()) {
+			G.enabled = true;
+		}
+		RB.useGravity = true;
+	}
+	
+	void Update () {
+		Vector3 LocVel = transform.InverseTransformDirection(RB.velocity);
+		transform.RotateAround(transform.position, transform.up * Input.GetAxisRaw("Horizontal"), Time.deltaTime * 75);
+		foreach (WheelCollider Wheel in GetComponentsInChildren<WheelCollider>()) {
+			Wheel.radius += Mathf.Sin (Time.time * first) * Time.deltaTime * second;
+			if (Input.GetAxisRaw("Vertical") > 0) {
+				if (LocVel.z < 15) {
+					Wheel.motorTorque = Input.GetAxisRaw("Vertical") * Speed;
+				} else {
+					LocVel.z = 15;
+				}
+			} else {
+				if (LocVel.z > -5) {
+					Wheel.motorTorque = Input.GetAxisRaw("Vertical") * (Speed / 4);
+				} else {
+					LocVel.z = -5;
+				}
+			}
+			WheelHit hit;
+			if (Wheel.GetGroundHit(out hit)) {
+				if (Input.GetAxisRaw("Vertical") == 0) {
+					RB.velocity -= new Vector3 (RB.velocity.x / BrakePower, 0, RB.velocity.z / BrakePower);
+				} else {
+					if (Mathf.Abs(LocVel.x) > 0) {
+						LocVel.x = LocVel.x / BrakePower;
+						RB.velocity = transform.TransformDirection(LocVel);
+					}
+				}
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.M)) {
+			Menu.SetActive (!Menu.active);
+		}
+	}
+}
