b368f2463ec2fb9b24b94d140fb19eee1e5c177d
JamesGagnon1 <43007253+JamesGagnon1@users.noreply.github.com>
2019-04-02 09:27:59 -0400

    Camera Changes

--- Car Tron/Assets/Scripts/Cam.cs
+++ Car Tron/Assets/Scripts/Cam.cs
@@ -8,14 +8,15 @@ public class Cam : MonoBehaviour {
 	public Transform Car;
 	public Vector3 Offset;
 	Vector3 TargetPos;
-	void Start () {
-		
-	}
 	
 	void Update () {
 		if (Car != null) {
 			transform.position = Vector3.Lerp (transform.position, new Vector3 (Car.position.x, Car.position.y + Offset.y, Car.position.z) + Car.forward * Offset.z, Time.deltaTime * 5);
 			transform.LookAt (Car.position + Car.transform.forward * 0.5f);
+		} else {
+			if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)) {
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			}
 		}
 	}
 	public void Restart() {
